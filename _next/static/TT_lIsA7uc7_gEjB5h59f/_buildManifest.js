ca6ddf6dfa731b97c32f194222d1704705b73586
thgil <leenf@tcd.ie>
2022-01-13 21:21:41 +0000

    Deploying to gh-pages from @ thgil/isjapanopen@027685e61e761a221628fb93dc9e7c6a281508cf 🚀

---
+++ _next/static/TT_lIsA7uc7_gEjB5h59f/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/784-d699b6028fdd0bf6.js","static/chunks/pages/index-63a456213b6a6ab9.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
